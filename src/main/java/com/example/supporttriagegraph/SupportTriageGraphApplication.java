bd6494abc1c8e4a2dec697194c77121abe973ae2
DonaldKisaka <davidkisaka4@gmail.com>
2026-07-02 11:48:09 +0300

    Added the main ChatClientConfig and Main application class

---
+++ src/main/java/com/example/supporttriagegraph/SupportTriageGraphApplication.java
@@ -0,0 +1,13 @@
+package com.example.supporttriagegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTriageGraphApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SupportTriageGraphApplication.class, args);
+  }
+
+}
